62ea3e8a4a63e2be5658e6b92de9ead642da7e1d
andreybglol <trimatahora@gmail.com>
2025-10-07 17:10:15 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FNaFle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
